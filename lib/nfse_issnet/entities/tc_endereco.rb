97ac72b80ccc1aa54e5c3ec90ec206f197132a14
Eduardo Scottini <eduardo.scottini@gmail.com>
2019-07-24 12:15:49 -0300

    retirado obrigatoriedade dos campos de endereço(considerando situação de estrangeiros)

--- lib/nfse_issnet/entities/tc_endereco.rb
+++ lib/nfse_issnet/entities/tc_endereco.rb
@@ -1,13 +1,13 @@
 module NfseIssnet
   module Entities
     class TcEndereco < BaseEntity
-      attribute? :endereco, Types::String.constrained(max_size: 125)
-      attribute? :cep, Types::Coercible::Integer.constrained(lteq: ('9' * 8).to_i)
-      attribute? :numero, Types::String.constrained(max_size: 10)
-      attribute? :complemento, Types::String.constrained(max_size: 60)
-      attribute? :bairro, Types::String.constrained(max_size: 60)
+      attribute? :endereco, Types::String.constrained(max_size: 125).optional
+      attribute? :cep, Types::Coercible::Integer.constrained(lteq: ('9' * 8).to_i).optional
+      attribute? :numero, Types::String.constrained(max_size: 10).optional
+      attribute? :complemento, Types::String.constrained(max_size: 60).optional
+      attribute? :bairro, Types::String.constrained(max_size: 60).optional
       attribute? :cidade, Types::String.constrained(max_size: 7)
-      attribute? :estado, Types::String.constrained(max_size: 2)
+      attribute? :estado, Types::String.constrained(max_size: 2).optional
     end
   end
 end
