fb11df71c3fe00418aa3734ae98b86303f1a5251
Joao Mafra <joaomafra21@gmail.com>
2019-10-23 17:02:57 -0300

    Tornando telefone opcional para o contato

--- lib/nfse_issnet/entities/tc_contato.rb
+++ lib/nfse_issnet/entities/tc_contato.rb
@@ -1,7 +1,7 @@
 module NfseIssnet
   module Entities
     class TcContato < BaseEntity
-      attribute :telefone, Types::String.constrained(max_size: 11)
+      attribute? :telefone, Types::String.constrained(max_size: 11).optional
       attribute :email, Types::String.constrained(max_size: 80)
     end
   end
